4d312b79b3c795a913e737b41c1541289e4b4a69
ryank53199 <rklingen@umich.edu>
2023-08-16 12:55:41 -0400

    info tag

--- Application/SplitSmart/screens/info.js
+++ Application/SplitSmart/screens/info.js
@@ -5,7 +5,8 @@ import React from 'react';
 const InfoScreen = () => {
     return (
         <View style={styles.container}>
-            <Text>Messages</Text>
+            <Text style={styles.header}>Messages</Text>
+            <Text style={styles.label}>People</Text>
         </View>
     );
 };
@@ -13,9 +14,24 @@ const InfoScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'left',
-        alignItems: 'left',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
+
+    header: {
+        position: 'absolute',
+        top: 10,
+        left: 10,
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+
+    label: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
 });
 
 
